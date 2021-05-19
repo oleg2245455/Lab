98d646361a63b27f5f7b8e6f066185778360f110
oleg2245455 <82165442+oleg2245455@users.noreply.github.com>
2021-05-19 13:56:04 +0300

    homeWork Factorial

---
+++ Labs/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs
+{
+    class Factorial
+    {
+         private static void Main(string[] args)
+        {
+            int num, fact = 1;
+            Console.WriteLine("Enter a number :");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial of " + num + "is :" + fact);
+            Console.ReadLine();
+        }
+
+    }
+}
