9a18ae1d8f8d3eff902547b7b449f621bff89f34
oleg2245455 <82165442+oleg2245455@users.noreply.github.com>
2021-05-19 16:20:09 +0300

    HomeWork

--- Labs/Program.cs
+++ Labs/Program.cs
@@ -2,9 +2,31 @@
 
 namespace Labs
 {
+    enum MyTask
+    {
+        Task1 = 1, Task2, 
+
+    }
     class Program
     {
         static void Main(string[] args)
+        {
+
+            
+            var result = MyTask.Task2;
+
+            switch (result)
+            {
+                case MyTask.Task2:
+                    {
+                        RunFactorial();
+                        break;
+                    }
+               
+            }
+        }
+               
+        private static void RunCalulator()
         {
             int num1 = 168; int num2 = 521;
 
@@ -50,5 +72,56 @@ namespace Labs
             Console.Write("Peress any key to close Calculator console app...");
             Console.ReadKey();
         }
+
+
+        private static void RunFactorial()
+        {
+            int num, fact = 1;
+            Console.WriteLine("Enter a number :");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial of " + num + "is :" + fact);
+            Console.ReadLine();
+        }
+
+        private static void RunRandom()
+        {
+
+            Random random = new Random();
+            int number = random.Next(1, 10);
+
+            while (true)
+            {
+                Console.WriteLine("Give me number:");
+                int userGuess = Int32.Parse(Console.ReadLine());
+
+                if (userGuess == number)
+                {
+                    break;
+                }
+                else if (userGuess > number && userGuess > 0 && userGuess < 11)
+                {
+                    Console.WriteLine("To High");
+                }
+                else if (userGuess < number && userGuess > 0 && userGuess < 11)
+                {
+                    Console.WriteLine("To Low");
+                }
+                else
+                {
+                    Console.WriteLine("Guess out bounds");
+                }
+
+
+            }
+            Console.WriteLine("Congratulation!!");
+        }
+
+
+
     }
 }
+
